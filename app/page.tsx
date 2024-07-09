e8335b83132b8c01652e4ae9a6370aec8095e89d
JbleMaker <jbojsun@gmail.com>
2024-07-09 23:00:06 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 import Image from "next/image";
+import logoB from "../logo/logotypeB_white.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="flex flex-col items-center justify-center p-6 min-h-screen ">
+      <div className="my-auto flex flex-col items-center gap-5 *:font-semibold ">
+        <Image src={logoB} alt="logo" className="w-50" />
+        <h1 className="text-center text-xl ">THREEMONTH</h1>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link href="/create-account" className="primary-btn text-lg py-2.5">
+          시작하기
+        </Link>
+        <div className="flex gap-3">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
