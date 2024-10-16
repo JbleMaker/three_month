c0a1ee7f90ca0a052021c4d38fadbb6335252386
JbleMaker <jbojsun@gmail.com>
2024-10-17 03:24:07 +0900

    commit password hashing

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,9 +1,40 @@
 "use server";
+import bcrypt from "bcrypt";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from "@/lib/constants";
 import { z } from "zod";
+import db from "@/lib/db";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkUsername = (username: string) => {
   return !username.includes("admin");
+};
+
+const checkUniqueUsername = async (username: string) => {
+  //닉네임 db 중복검사
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  //이메일 db 중복검사
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
 };
 
 const checkPassword = ({
@@ -27,9 +58,14 @@ const formSchema = z
       .max(10, "닉네임이 너무 깁니다.")
       .trim()
       .toLowerCase()
-      .refine(checkUsername, "사용할 수 없는 이름 입니다."),
+      .refine(checkUsername, "사용할 수 없는 이름 입니다.")
+      .refine(checkUniqueUsername, "이미 사용중인 닉네임입니다."),
 
-    email: z.string().email().toLowerCase(),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(checkUniqueEmail, "이미 사용중인 이메일입니다."),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH, "비밀번호는 8자리 이상이여야 합니다.")
@@ -45,6 +81,7 @@ const formSchema = z
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
+  console.log(cookies());
   const data = {
     username: formData.get("username"),
     email: formData.get("email"),
@@ -52,10 +89,31 @@ export async function createAccount(prevState: any, formData: FormData) {
     password_confirm: formData.get("password_confirm"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    //비밀번호 hash
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    //사용자 db 저장
+    //사용자 로그인
+    const session = await getIronSession(cookies(), {
+      cookieName: "login-authentication",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    //@ts-ignore
+    session.id = user.id;
+    await session.save();
+    redirect("/profile");
   }
 }
